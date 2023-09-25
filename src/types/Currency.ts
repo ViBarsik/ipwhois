82fab100812254b0a3b114c3969c8a8f996c1c74
Vitalii Pylypenko <ilativ.oknepilip@gmail.com>
2023-09-25 17:14:57 +0200

    Eslint format

--- src/types/Currency.ts
+++ src/types/Currency.ts
@@ -1,23 +1,23 @@
 export type CurrencyProps = {
-	name?: string;
-	code?: string;
-	symbol?: string;
-	plural?: string;
-	exchange_rate?: number;
-}
+  name?: string;
+  code?: string;
+  symbol?: string;
+  plural?: string;
+  exchange_rate?: number;
+};
 
 export class Currency {
-	name: string;
-	code: string;
-	symbol: string;
-	plural: string;
-	exchange_rate: number;
+  name: string;
+  code: string;
+  symbol: string;
+  plural: string;
+  exchange_rate: number;
 
-	constructor(props: CurrencyProps) {
-		this.name = props.name;
-		this.code = props.code;
-		this.symbol = props.symbol;
-		this.plural = props.plural;
-		this.exchange_rate = props.exchange_rate;
-	}
+  constructor(props: CurrencyProps) {
+    this.name = props.name;
+    this.code = props.code;
+    this.symbol = props.symbol;
+    this.plural = props.plural;
+    this.exchange_rate = props.exchange_rate;
+  }
 }
--- src/types/Flag.ts
+++ src/types/Flag.ts
@@ -1,17 +1,17 @@
 export type FlagProps = {
-	img?: string;
-	emoji?: string;
-	emoji_unicode?: string;
-}
+  img?: string;
+  emoji?: string;
+  emoji_unicode?: string;
+};
 
 export class Flag {
-	img: string;
-	emoji: string;
-	emoji_unicode: string;
+  img: string;
+  emoji: string;
+  emoji_unicode: string;
 
-	constructor(props: FlagProps) {
-		this.img = props.img;
-		this.emoji = props.emoji;
-		this.emoji_unicode = props.emoji_unicode;
-	}
+  constructor(props: FlagProps) {
+    this.img = props.img;
+    this.emoji = props.emoji;
+    this.emoji_unicode = props.emoji_unicode;
+  }
 }
